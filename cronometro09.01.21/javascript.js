d1e6c1859e13f1bfcde4a01b8452dadac969e3eb
Margarita Rodriguez <margareth1893@gmail.com>
2021-01-11 18:18:30 -0500

    ultimo

--- cronometro09.01.21/javascript.js
+++ cronometro09.01.21/javascript.js
@@ -11,6 +11,7 @@ window.onload = () => {
   parar = document.getElementById('parar');
   grabar = document.getElementById('grabar');
   almacenarTiempos = document.getElementById('almacenarTiempos');
+
   event();
 };
 
@@ -81,3 +82,289 @@ function grabarContador() {
     almacenarTiempos.appendChild(p);
   }
 }
+//RESPONSIVE//
+
+h = 0;
+m = 0;
+s = 0;
+mls = 0;
+timeStarted = 0;
+
+temporizador = document.getElementById('temporizador');
+play = document.getElementById('play');
+reset = document.getElementById('reset');
+pause = document.getElementById('pause');
+record = document.getElementById('record');
+saveTime = document.getElementById('saveTime');
+
+init();
+
+function init() {
+  play.addEventListener('click', playContador);
+  reset.addEventListener('click', resetContador);
+  pause.addEventListener('click', pauseContador);
+  record.addEventListener('click', recordContador);
+}
+
+function count() {
+  let ht, mt, st, mlst;
+  mls++;
+
+  if (mls > 99) {
+    s++;
+    mls = 0;
+  }
+  if (s > 59) {
+    m++;
+    s = 0;
+  }
+  if (m > 59) {
+    h++;
+    m = 0;
+  }
+  if (h > 24) h = 0;
+
+  mlst = ('0' + mls).slice(-2);
+  st = ('0' + s).slice(-2);
+  mt = ('0' + m).slice(-2);
+  ht = ('0' + h).slice(-2);
+
+  temporizador.innerHTML = `${ht}:${mt}:${st}.${mlst}`;
+}
+
+function playContador() {
+  count();
+  timeStarted = setInterval(count, 10);
+  play.removeEventListener('click', playContador);
+}
+function pauseContador() {
+  clearInterval(timeStarted);
+  play.addEventListener('click', playContador);
+}
+
+function resetContador() {
+  clearInterval(timeStarted);
+  temporizador.innerHTML = '00:00:00.00';
+  h = 0;
+  m = 0;
+  s = 0;
+  mls = 0;
+  play.addEventListener('click', playContador);
+  while (saveTime.firstChild) {
+    saveTime.removeChild(saveTime.firstChild);
+  }
+}
+
+function recordContador() {
+  if (temporizador.textContent === '00:00:00.00') {
+    console.log('click en el botón play');
+  } else {
+    let p = document.createElement('ul');
+    p.innerHTML = `
+        <li>temporizador: ${h}:${m}:${s}.${mls}</li>
+    `;
+    saveTime.appendChild(p);
+  }
+}
+
+//if (verificador == false) {
+//  intervalo = setInterval(function () {
+//    tiempo += 0.01;
+//  temporizador.innerHTML = tiempo.toFixed(2);
+// }, 10);
+// verificador = true;
+// } else {
+// verificador = false;
+// clearInterval(intervalo);
+// }
+// }
+
+//function resetearContador() {
+// verificador = false;
+// tiempo = 0;
+// temporizador.innerHTML = tiempo + ':00';
+// clearInterval(intervalo);
+// while (almacenarTiempos.firstChild) {
+// almacenarTiempos.removeChild(almacenarTiempos.firstChild);
+// }
+// }
+
+//function grabarContador() {
+//if (temporizador.textContent === '00:00:00') {
+//console.log('click en el botón iniciar');
+//} else {
+//let p = document.createElement('ul');
+//p.innerHTML = `
+//  <li>Tiempo : ${tiempo.toFixed(2)}</li>
+//  `;
+// almacenarTiempos.appendChild(p);
+// }
+//}
+
+// seconds = 0;
+// s = document.getElementById('seconds');
+// m = document.getElementById('minutes');
+// h = document.getElementById('hours');
+
+// cronometer = setInterval(function () {
+// seconds++;
+//  secs = seconds;
+//  mins = 0;
+//  hours = 0;
+
+//  while (secs>=60) {
+//    mins++;
+//  secs-=60;
+//  }
+//  while (mins <= 60) {
+//    hours++;
+//    mins -= 60;
+// }
+
+//  if (hours < 10) h.innerHTML = '0' + hours;
+//  else h.innerHTML = hours;
+//  if (mins<10) m.innerHTML = '0' + mins;
+//  else m.innerHTML = mins;
+//  if (secs < 10) s.innerHTML = '0' + secs;
+//  else s.innerHTML = secs;
+
+//  Total_secs = secs;
+//  Total_mins = mins;
+//  Total_hours = hours;
+// }, 1000);
+// }
+
+// function promiseSquare(val){
+//     return Promise.resolve(val * val);
+// }
+
+// function promiseDouble(val){
+//     return Promise.resolve(val + val);
+// }
+
+// function filterFunction(val){
+//     if ( val > 50)
+//         return true;
+//     else
+//         return false;
+// }
+
+// var myPromise1 = Promise.resolve(500);
+// var myPromise2 = promiseSquare(10);
+// var myPromise3 = promiseDouble(20);
+
+// Promise.all([myPromise1,myPromise2,myPromise3]).then(function(val){
+//     var result = val.filter(filterFunction);
+//     return result;
+
+// }).then(function(val){
+//     var temp = 0;
+//     for(var i = 0; i < val.length; i++){
+//         temp += val[i]
+//     }
+//     console.log(temp);
+
+// }).catch(function(err){
+//     console.log("Error: " + err)
+// });
+
+//if (verificador == false) {
+//  intervalo = setInterval(function () {
+//    tiempo += 0.01;
+//  temporizador.innerHTML = tiempo.toFixed(2);
+// }, 10);
+// verificador = true;
+// } else {
+// verificador = false;
+// clearInterval(intervalo);
+// }
+// }
+
+//function resetearContador() {
+// verificador = false;
+// tiempo = 0;
+// temporizador.innerHTML = tiempo + ':00';
+// clearInterval(intervalo);
+// while (almacenarTiempos.firstChild) {
+// almacenarTiempos.removeChild(almacenarTiempos.firstChild);
+// }
+// }
+
+//function grabarContador() {
+//if (temporizador.textContent === '00:00:00') {
+//console.log('click en el botón iniciar');
+//} else {
+//let p = document.createElement('ul');
+//p.innerHTML = `
+//  <li>Tiempo : ${tiempo.toFixed(2)}</li>
+//  `;
+// almacenarTiempos.appendChild(p);
+// }
+//}
+
+// seconds = 0;
+// s = document.getElementById('seconds');
+// m = document.getElementById('minutes');
+// h = document.getElementById('hours');
+
+// cronometer = setInterval(function () {
+// seconds++;
+//  secs = seconds;
+//  mins = 0;
+//  hours = 0;
+
+//  while (secs>=60) {
+//    mins++;
+//  secs-=60;
+//  }
+//  while (mins <= 60) {
+//    hours++;
+//    mins -= 60;
+// }
+
+//  if (hours < 10) h.innerHTML = '0' + hours;
+//  else h.innerHTML = hours;
+//  if (mins<10) m.innerHTML = '0' + mins;
+//  else m.innerHTML = mins;
+//  if (secs < 10) s.innerHTML = '0' + secs;
+//  else s.innerHTML = secs;
+
+//  Total_secs = secs;
+//  Total_mins = mins;
+//  Total_hours = hours;
+// }, 1000);
+// }
+
+// function promiseSquare(val){
+//     return Promise.resolve(val * val);
+// }
+
+// function promiseDouble(val){
+//     return Promise.resolve(val + val);
+// }
+
+// function filterFunction(val){
+//     if ( val > 50)
+//         return true;
+//     else
+//         return false;
+// }
+
+// var myPromise1 = Promise.resolve(500);
+// var myPromise2 = promiseSquare(10);
+// var myPromise3 = promiseDouble(20);
+
+// Promise.all([myPromise1,myPromise2,myPromise3]).then(function(val){
+//     var result = val.filter(filterFunction);
+//     return result;
+
+// }).then(function(val){
+//     var temp = 0;
+//     for(var i = 0; i < val.length; i++){
+//         temp += val[i]
+//     }
+//     console.log(temp);
+
+// }).catch(function(err){
+//     console.log("Error: " + err)
+// });
